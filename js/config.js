75b2f71de62df9f85a05475b569fa721efa24b5c
hadar <hadar.hav@gmail.com>
2018-11-22 08:16:48 +0200

    toggle button

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 
 
 const coinsApi = "../templates/coins.html"
+const coinsDetlailsAPI = "https://api.coingecko.com/api/v3/coins/";
 
 const DOM = function () {
 
--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,46 @@
+$(window).on("scroll", function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+
+    else {
+        $('nav').removeClass('black');
+    }
+})
+
+const createToggle = (target) => {
+    new DG.OnOffSwitch({
+        el: `#on-off-switch-${target}`,
+        height: 25,
+        trackColorOn: '#ffc107',
+        trackColorOff: '#666',
+        trackBorderColor: '#555',
+        textColorOff: '#fff',
+        textOn: '',
+        textOff: ''
+    })
+}
+
 
 
 const CreateCoinCard = (coin) => {
-    let card = `<div class="card mt-3" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${coin.symbol}</h5>
-            <p class="card-text">${coin.name}</p>
-            <a href="" class="btn btn-warning">More Info</a>
-        </div>
-    </div>`
+    let card = `<div class="card d-flex mt-3" style="width: 18rem; z-index: 1">
+        <input type="hidden" class="ml-auto" id="on-off-switch-${coin.symbol}" value="0">
+            <div class="card-body">
+                <h5 class="card-title">${coin.symbol}</h5>
+                <p class="card-text">${coin.name}</p>
+                <a href="" class="btn btn-warning">More Info</a>
+            </div>
+        </div>`;
+
 
     return card;
 }
 
 const drawCards = (coins) => {
     for (let i = 0; i < coins.length; i++) {
-        let currentCard = CreateCoinCard(coins[i])
-        DOM.coinsPage.append(currentCard);
+        DOM.coinsPage.append(CreateCoinCard(coins[i]));
+        createToggle(coins[i].symbol)
 
     }
     DOM.mainContainer.height(DOM.coinsPage.height() + 300)
